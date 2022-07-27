3702b51638f8f4fd41ac3acc91c7f3ba623ca472
Alima Roman <59642740+matyevaa@users.noreply.github.com>
2022-07-26 23:32:15 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ class App extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/star-wars" component={MainPage} />
-          <Route path="/people" component={PeoplePage} />
-          <Route path="/planets" component={PlanetsPage} />
-          <Route path="/films" component={FilmsPage} />
+          <Route path="/star-wars/people" component={PeoplePage} />
+          <Route path="/star-wars/planets" component={PlanetsPage} />
+          <Route path="/star-wars/films" component={FilmsPage} />
           <Route exact path="/404" component={ErrorPage} />
           <Route path="*"><Redirect to="/404" /></Route> /* put on the very bottom */
         </Switch>
